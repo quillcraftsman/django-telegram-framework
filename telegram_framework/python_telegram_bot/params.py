7032ae71b6f7ea063ad0df565e035839e2f4263b
quillcraftsman <quill@craftsman.lol>
2026-03-16 17:40:42 +0300

    cover python_telegram_bot/params.py

--- telegram_framework/python_telegram_bot/params.py
+++ telegram_framework/python_telegram_bot/params.py
@@ -1,6 +1,7 @@
 import functools
+from typing import Callable
 from django.urls.resolvers import RoutePattern
-from telegram_framework.messages.call.call import Call
+from telegram_framework.messages import Call, Message
 
 
 def _match_function(pattern, message_data: str):
@@ -8,10 +9,10 @@ def _match_function(pattern, message_data: str):
     return route_pattern.match(message_data)
 
 
-def _prepare_command_handler(handler_function, match_function):
+def _prepare_command_handler(handler_function: Callable, match_function: Callable):
 
     @functools.wraps(handler_function)
-    def inner(bot, message, *args, **kwargs):
+    def inner(bot, message: Message, *args, **kwargs):
         _, _, params = '', (), {}
         _, _, params = match_function(message)
         kwargs = kwargs | params
@@ -20,7 +21,7 @@ def _prepare_command_handler(handler_function, match_function):
     return inner
 
 
-def _prepare_call_handler(handler_function, match_function):
+def _prepare_call_handler(handler_function: Callable, match_function: Callable):
     # Для Dummy call и command одинаковые
     return _prepare_command_handler(handler_function, match_function)
 
@@ -50,21 +51,21 @@ def _get_call_match_function(pattern):
 
 def _get_command_match_function(pattern):
 
-    def inner(message):
+    def inner(message: Message):
         message_data = message.text
         return _match_function(pattern, message_data)
 
     return inner
 
 
-def get_param_call_handler(params_pattern, handler):
+def get_param_call_handler(params_pattern, handler: Callable):
     match_function = _get_call_match_function(params_pattern)
     handler = _prepare_call_handler(handler, match_function)
     filter_function = _get_filter_function(params_pattern)
     return handler, filter_function
 
 
-def get_param_command_handler(params_pattern, handler):
+def get_param_command_handler(params_pattern, handler: Callable):
     match_function = _get_command_match_function(params_pattern)
     handler = _prepare_command_handler(handler, match_function)
     filter_function = _get_filter_function(params_pattern)

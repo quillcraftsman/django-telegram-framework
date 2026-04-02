56ca9e18edb6e69d4c9b2153c74ecb5141afc9d9
quillcraftsman <quill@craftsman.lol>
2026-04-02 13:21:20 +0300

    revrite test_views with pytest

--- demo/tests/test_views.py
+++ demo/tests/test_views.py
@@ -1,44 +1,26 @@
+import pytest
 from django.urls import reverse
-from dry_tests import SimpleTestCase, Request, TrueResponse, Context
 from demo.models import create_info_text
 
 
-class TestIndexView(SimpleTestCase):
-
-    def test_get(self):
-        request = Request(reverse('demo:index'))
-        true_response = TrueResponse(
-            status_code=200,
-        )
-        current_response = request.get_response(self.client)
-        self.assertTrueResponse(current_response, true_response)
+def test_index_view(client):
+    response = client.get(reverse('demo:index'))
+    assert response.status_code == 200
 
 
-class TestCommandView(SimpleTestCase):
-
-    def test_get(self):
-        commands = ['start', 'help']
-        for command in commands:
-            with self.subTest(f'Test for /{command} command'):
-                request = Request(reverse(f'demo:{command}'))
-                text = create_info_text(f'/{command}')
-                true_response = TrueResponse(
-                    status_code=200,
-                    context=Context(
-                        items={'text': text}
-                    ),
-                    content_values=[text]
-                )
-                current_response = request.get_response(self.client)
-                self.assertTrueResponse(current_response, true_response)
+def test_links_view(client):
+    response = client.get(reverse('demo:links'))
+    assert response.status_code == 200
 
 
-class TestLinksView(SimpleTestCase):
-
-    def test_get(self):
-        request = Request(reverse('demo:links'))
-        true_response = TrueResponse(
-            status_code=200,
-        )
-        current_response = request.get_response(self.client)
-        self.assertTrueResponse(current_response, true_response)
+@pytest.mark.parametrize('command', [
+    'start',
+    'help',
+])
+def test_command_view(client, command):
+    response = client.get(reverse(f'demo:{command}'))
+    assert response.status_code == 200
+    context = response.context
+    assert 'text' in context
+    text = create_info_text(f'/{command}')
+    assert context['text'] == text
